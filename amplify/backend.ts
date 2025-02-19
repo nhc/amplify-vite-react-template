534bf858f09b2064a32483fbc6b2e899170a87c6
Neil Charlton <neil@bleepsystems.com>
2025-02-19 14:38:41 +0000

    wed-19-14:00 working

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,12 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { siteStorage, cvStorage, jobSpecStorage } from "./storage/resource";
+import { siteStorage } from "./storage/resource";
 
 defineBackend({
   auth,
   data,
   siteStorage,
-  cvStorage,
-  jobSpecStorage,
+  // cvStorage,
+  // jobSpecStorage,
 });
